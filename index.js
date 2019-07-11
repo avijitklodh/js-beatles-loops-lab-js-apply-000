e1c6c027e2ca52379ab20eb67bc44966b017705e
Avijit Lodh <avijitklodh@gmail.com>
2019-07-11 15:05:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function theBeatlesPlay(players, instruments) {
     return allPlayers;
 }
 
+<<<<<<< HEAD
 function johnLennonFacts(facts) {
 
     var newFacts = [];
@@ -16,6 +17,17 @@ function johnLennonFacts(facts) {
         i++;
     }
     return newFacts;
+=======
+function johnLennonFacts() {
+    const facts = ["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i]);
+        i++;
+    }
+    return newFacts + "!!!";
+>>>>>>> ca3d6ff2498f3d330843de6f7595d58136b6ae9a
 }
 
 function iLoveTheBeatles(num) {
